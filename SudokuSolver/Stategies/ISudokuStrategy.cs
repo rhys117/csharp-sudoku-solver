5238263f568efe074a2ff6591cf52b3c02c9eb35
rhys117 <rhys117@gmail.com>
2019-05-23 21:59:45 +1000

    sudoku solver engine added

---
+++ SudokuSolver/Stategies/ISudokuStrategy.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SudokuSolver.Stategies
+{
+    interface ISudokuStrategy
+    {
+        int[,] Solve(int[,] sudokuBoard);
+    }
+}
---
+++ SudokuSolver/Workers/SudokuBoardDisplayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SudokuSolver.Workers
+{
+    class SudokuBoardDisplayer
+    {
+        public void Display(string title, int[,] sudokuBoard)
+        {
+            if (!title.Equals(string.Empty)) Console.WriteLine($"{title} {Environment.NewLine}");
+
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                Console.Write("|");
+                for (int col = 0; row < sudokuBoard.GetLength(1); row++)
+                {
+                    Console.WriteLine($"{sudokuBoard[row, col]}|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
